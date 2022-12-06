1ad7809084c26b81e7ceb175bd9731f912df4884
sunhong kim <eoaud447@naver.com>
2022-12-07 01:56:54 +0900

    video

--- src/Project.js
+++ src/Project.js
@@ -1,7 +1,7 @@
 import React, { useState,useRef } from 'react'
 import {FaCheck,FaGithub,FaChrome } from 'react-icons/fa'
 import {FiArrowDownRight,FiArrowUpLeft} from "react-icons/fi"
-import { Player } from 'video-react';
+import ReactPlayer from 'react-player/lazy';
 import './style/project.scss'
 
 function Project({timgSrc,mimgSrc,imgSrc,imgAlt,title,subTitle,text,language,GitHref,URLHref,FaChevronRight,FaChevronLeft,project}) {
@@ -22,11 +22,19 @@ function Project({timgSrc,mimgSrc,imgSrc,imgAlt,title,subTitle,text,language,Git
                     <img src={mimgSrc} alt={imgAlt} className='m_img' />
                 </div>
                 <div className='kakao_video'>
-                <Player
-                    playsInline
-                    poster="img/kakao.png"
-                    src="video/kakao.mp4"
-                    />
+                <ReactPlayer
+                    className='react-player'
+                    url={'video.kakao.mp4'}   
+                    width='100%'         // 플레이어 크기 (가로)
+                    height='100%'        // 플레이어 크기 (세로)
+                    playing={true}        // 자동 재생 on
+                    muted={true}          // 자동 재생 on
+                    controls={false}       // 플레이어 컨트롤 노출 여부
+                    light={false}         // 플레이어 모드
+                    pip={true}            // pip 모드 설정 여부
+                    poster={'kakao.png'}   // 플레이어 초기 포스터 사진
+                    onEnded={() => handleVideo()}  // 플레이어 끝났을 때 이벤트
+                />
                 </div>
                 <div className='text_info'>
                 <h3 className='title'>{title}</h3>
